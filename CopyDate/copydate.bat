4e58a2fadac9d0d77f0c9032e6755f4756e69c4e
R-Tatara <rabumel.dpl@gmail.com>
2020-05-31 21:33:49 +0900

    Add comment

--- CopyDate/copydate.bat
+++ CopyDate/copydate.bat
@@ -1,4 +1,8 @@
 @echo off
+rem ----------------------------------------
+rem 今日の日付をクリップボードにコピーする
+rem ----------------------------------------
+
 setlocal
 pushd "%~dp0"
 
@@ -7,3 +11,4 @@ rem echo %date:~0,4%%date:~5,2%%date:~8,2% | clip
 
 popd
 endlocal
+exit
